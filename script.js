20fbf11825a7efab53a18bd90391c2bd6946e871
Arnavhate <argadadane@gmail.com>
2025-02-19 13:57:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const downloadButton = document.getElementById('downloadBtn');
+    
+    if (downloadButton) {
+        downloadButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            window.location.href = 'Lessmerzied.rar';
+        });
+    }
+});
