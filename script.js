512e6ebdf8ab0670df9d384b6f2019a2a49433c6
Arnavhate <argadadane@gmail.com>
2025-02-19 13:55:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const downloadButton = document.getElementById('downloadBtn');
-    
-    if (downloadButton) {
-        downloadButton.addEventListener('click', (event) => {
-            event.preventDefault();
-            alert('Your download is starting...');
-            window.location.href = 'Lessmerzied.zip';
-        });
-    }
-});
+
